83609ef7d9fed2d6eeb91311451d2d06a37cdf85
Hassan Ammar <h.ammar@hotmail.de>
2024-01-04 14:56:29 +0100

    added json array

---
+++ script.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        'question': 'Wer hat HTML erfunden?',
+        'answer_1': 'Robbie Williams',
+        'answer_2': 'Lady Gaga',
+        'answer_3': 'Tim Berners-Lee',
+        'answer_4': 'Justin Bieber',
+        'right_answer': '3'
+    },
+    {
+        'question': 'Was bedeutet HTML?',
+        'answer_1': 'Hypertext Markup Language',
+        'answer_2': 'High Tech Modern Language',
+        'answer_3': 'Hyperlink and Text Markup Language',
+        'answer_4': 'Home Tool Markup Language',
+        'right_answer': '1'
+    },
+    {
+        'question': 'Welches HTML-Element wird für die Überschriften von Abschnitten verwendet?',
+        'answer_1': '<header>',
+        'answer_2': '<h1>',
+        'answer_3': '<title>',
+        'answer_4': '<section>',
+        'right_answer': '2'
+    },
+    {
+        'question': 'Was ist der Zweck des HTML-Elements <a>?',
+        'answer_1': 'Für Absätze',
+        'answer_2': 'Für Bilder',
+        'answer_3': 'Für Hyperlinks',
+        'answer_4': 'Für Listen',
+        'right_answer': '3'
+    },
+    {
+        'question': 'Welche Dateiendung wird für HTML-Dokumente verwendet?',
+        'answer_1': '.exe',
+        'answer_2': '.html',
+        'answer_3': '.txt',
+        'answer_4': '.css',
+        'right_answer': '2'
+    }
+];
+    
